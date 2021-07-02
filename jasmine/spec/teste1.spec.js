05f11c0282431bae14d32d08c043c8e1a4db4856
leoportogtr86 <leoporto_studio@hotmail.com>
2021-07-02 07:25:23 -0300

    primeiro teste com jasmine

---
+++ jasmine/spec/teste1.spec.js
@@ -0,0 +1,7 @@
+describe('Primeiro teste escrito com Jasmine', () => {
+
+    it('Deve retornar a frase "Hello World!"', () => {
+
+        expect(teste1()).toBe('Hello World!')
+    });
+});
---
+++ jasmine/src/teste1.js
@@ -0,0 +1,4 @@
+let teste1 = () => {
+
+    return 'Hello World!'
+}
